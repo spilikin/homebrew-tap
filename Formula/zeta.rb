571bf36db08e4c82fe090300ed8fc7e4600b48b0
Sergej Suskov <108782+spilikin@users.noreply.github.com>
2026-08-13 09:19:30 +0200

    zeta 0.10.0

--- Formula/zeta.rb
+++ Formula/zeta.rb
@@ -1,8 +1,8 @@
 class Zeta < Formula
   desc "Swiss-army-knife Zeta client and tools"
   homepage "https://github.com/gematik/zeta-cli"
-  url "https://github.com/gematik/zeta-cli/releases/download/v0.9.3/zeta-0.9.3.tar.gz"
-  sha256 "0f04137d1547bfb48f8904fc1f6687ec5b8ae8c532ca6433351a64d048a487f9"
+  url "https://github.com/gematik/zeta-cli/releases/download/v0.10.0/zeta-0.10.0.tar.gz"
+  sha256 "11ff60218c7c6be236f04e5a248b3a2249e639e6f64450099819f53cdcf82cc7"
   license "Apache-2.0"
 
   depends_on "openjdk@21"
